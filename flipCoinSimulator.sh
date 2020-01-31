581a5d9ccbf83a8ed1125a9daae8e81c702ab858
supriyaphalle <supriyaphalle@gmail.com>
2020-01-31 16:10:10 +0530

    ADDED:Added percentage for occuring singlet

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,14 +1,25 @@
-#! /bin/bash -x
-
-echo "Welcome to the FlipCoin Simulator"
-
 #constant
 Head=0;
 Tail=1;
+loop=10;
+Singlet[Head]=0
+Singlet[Tail]=0
 
-if (( $((RANDOM%2)) == $Head ))
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+for ((i=0;i<$loop;i++))
+do
+	if (( $((RANDOM%2)) == $Head ))
+	then
+	#	echo "Head"
+		Singlet[Head]=$((${Singlet[Head]}+1))
+	else
+	#	echo "Tail"
+			Singlet[Tail]=$((${Singlet[Tail]}+1))
+	fi
+
+done
+
+echo " all calues " ${Singlet[@]}
+echo ${!Singlet[@]}
+echo "Percentage of Singlet Combination"
+echo " percentage of Occuring Head = $((${Singlet[Head]} * 100/$loop))"
+echo " percentage of Occuring Tail = $((${Singlet[Tail]} * 100/$loop))"
