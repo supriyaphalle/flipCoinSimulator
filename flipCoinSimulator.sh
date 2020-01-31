54623e62b5a6bf5abf1ea4bd8e8470b2a1652bce
supriyaphalle <supriyaphalle@gmail.com>
2020-01-31 16:02:49 +0530

    ADDED: Added logic for Singlet fliping coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,14 @@
-echo "Welcome to the Flipcoin Simulator"
+#! /bin/bash -x
 
+echo "Welcome to the FlipCoin Simulator"
+
+#constant
+Head=0;
+Tail=1;
+
+if (( $((RANDOM%2)) == $Head ))
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
