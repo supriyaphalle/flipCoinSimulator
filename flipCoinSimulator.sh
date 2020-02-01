a061c847d7e4eca98d1e2f1b55f5fddf4c458110
supriyaphalle <supriyaphalle@gmail.com>
2020-02-01 11:40:56 +0530

    REMOVED: Removed extra commented part

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,38 +1,3 @@
-#constant
-#Head=0;
-#Tail=1;
-#loop=20;
-#Singlet[Head]=0
-#Singlet[Tail]=0
-#
-
-#while ((1))
-#do
-#	echo "Welcome To the Flipcoin Simulator"
-#	read -p " Do you want to play 1/0" option
-#	if(
-#	for ((i=0;i<$loop;i++))
-#	do
-#		if (( $((RANDOM%2)) == $Head ))
-#		then
-		#	echo "Head"
-#			Singlet[Head]=$((${Singlet[Head]}+1))
-#		else
-		#	echo "Tail"
-#			Singlet[Tail]=$((${Singlet[Tail]}+1))
-#		fi
-
-#	done
-
-#	echo " all calues " ${Singlet[@]}
-#	echo ${!Singlet[@]}
-#	echo "Percentage of Singlet Combination"
-#	echo " percentage of Occuring Head = $((${Singlet[Head]} * 100/$loop))"
-#	echo " percentage of Occuring Tail = $((${Singlet[Tail]} * 100/$loop))"
-
-
-
-
 #!/bin/bash 
 
 declare -A flips
